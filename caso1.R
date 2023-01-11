654c8b49a468aba181fe8e4703cb49858a83f670
marinasainz <msb1014@alu.ubu.es>
2023-01-11 19:01:50 +0100

    CASO 1: relación entre la aparición de enfermedades frente al sexo

--- caso1.R
+++ caso1.R
@@ -1,32 +1,54 @@
-#CASO 1 DEL ESTUDIO: RELACIÓN ENTRE EL SEXO Y LA APARICION DE ENFERMEDADES
-
+#CASO 1: SEXO Y ENFERMEDADES: ENFERMEDADES DE MUJERES VS HOMBRES
 #a. mujeres
 
 library(ggplot2)
-#selecciono que sean mujeres(por sexo)
+#selecciono que sean mujeres(por sexo), por edad y por actividad
 tablasCaso4muj<- tablasCompletas[tablasCompletas$`Sexo`=="Mujeres" ,]
 
-View(tablasCaso4muj)
+#View(tablasCaso4muj)
 #elimino duplicados
-caso4SinDup<-distinct(tablasCaso4muj)
-View(caso4SinDup)
-#grafico
-grafcaso4<-ggplot(data = caso4SinDup, aes(x=`Enfermos en porcentaje`, y =`Enfermedades`)) + geom_point(colour = "palevioletred2") + geom_smooth(method = "lm", formula = y~poly(x,1), colour = "red", se = TRUE)
-grafcaso4
+caso4SinDupMuj<-distinct(tablasCaso4muj)
+#View(tablasCaso4muj)
+
+
+#GRAFICO BIEN?SIIIIIIIIIIIIIIIIIIIIIIIIIII
+
+library(ggplot2)
+graficaMujeres<-ggplot(data = caso4SinDupMuj, aes(x =`Enfermos en porcentaje`, y = `Enfermedades`)) + 
+  geom_bar(stat="identity", position="dodge", colour ="black", fill = "red")+
+  labs( title = "ENFERMEDADES EN MUJERES",
+        x = "Porcentaje de enfermos",
+        y = "Enfermedad")+
+  theme (plot.title = element_text(size=rel(1),   color="black")) 
+#graficaMujeres
+
 
 #b.hombres
 
-library(ggplot2)
+#library(ggplot2)
 #selecciono que sean mujeres(por sexo), por edad y por actividad
 tablasCaso4hom<- tablasCompletas[tablasCompletas$`Sexo`=="Hombres" ,]
 
-View(tablasCaso4hom)
+#View(tablasCaso4hom)
 #elimino duplicados
 caso4homSinDup<-distinct(tablasCaso4hom)
-View(caso4homSinDup)
-#grafico
-grafcaso4hom<-ggplot(data = caso4homSinDup, aes(x=`Enfermos en porcentaje`, y =`Enfermedades`)) + geom_point(colour = "palevioletred2") + geom_smooth(method = "lm", formula = y~poly(x,1), colour = "red", se = TRUE)
-grafcaso4hom
+
+#View(caso4homSinDup)
+
+
+
+
+library(ggplot2)
+graficaHombres<-ggplot(data = caso4homSinDup, aes(x =`Enfermos en porcentaje`, y = `Enfermedades`)) + 
+  geom_bar(stat="identity", position="dodge", colour ="black", fill = "red")+
+  labs( title = "ENFERMEDADES EN HOMBRES",
+        x = "Porcentaje de enfermos",
+        y = "Enfermedad")+
+  theme (plot.title = element_text(size=rel(1),   color="black")) 
+#graficaHombres
+
+
+
 
 
 
